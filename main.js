627c5d2538993bf5a4c2d6b9e43b1e768c83192e
boswel <s.luerzel@gmail.com>
2023-11-23 22:04:10 +0100

    created hue sliders

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
-import { Ball } from "./Ball.js";
+import { Ball } from './Ball.js'
 
-let canvas = document.querySelector("canvas");
-let context = canvas.getContext("2d");
+let canvas = document.querySelector('canvas');
+let context = canvas.getContext('2d');
+let slides = document.querySelectorAll('input');
+
+for (let slide of slides) {
+    slide.addEventListener('change', createBalls);
+}
+
+function calculateRangeValues() {
+    let min = slides[0].value < slides[1].value ? +slides[0].value : +slides[1].value;
+    let diff = Math.abs(slides[0].value - slides[1].value);
+    return { diff: diff, min: min }
+}
 
 function getRandomColor() {
-    let hue = Math.floor(Math.random() * 255);
+    let hue = Math.floor(Math.random() * calculateRangeValues()['diff']) + calculateRangeValues()['min'];
     let saturation = Math.floor(Math.random() * 100);
     let lightness = Math.floor(Math.random() * 50) + 25;
 
@@ -13,17 +24,16 @@ function getRandomColor() {
 
 let ballPool = [];
 
-for (let i = 0; i < 100; i++) {
-    let ball = new Ball(context, getRandomColor(), Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 2 * Math.PI);
-    ballPool.push(ball);
+function createBalls() {
+    ballPool = [];      // Q
+    for (let i = 0; i < 100; i++) {
+        let ball = new Ball(context, getRandomColor(), Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 2 * Math.PI);
+        ballPool.push(ball);
+    }
 }
 
-// let ball2 = new Ball(context, "green", Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 2 * Math.PI);
-// let ball3 = new Ball(context, "red", Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 2 * Math.PI);
-// let ball4 = new Ball(context, "purple", Math.random() * canvas.width, Math.random() * canvas.height, Math.random() * 2 * Math.PI);
 
-
-
+window.onload = createBalls;
 
 function gameLoop() {
     context.reset();
