7d87f46e8db27f5d49c2ea9ef0118f1288d8e01b
boswel <s.luerzel@gmail.com>
2023-11-24 20:33:17 +0100

    working slides for hue, saturation, lightness

--- main.js
+++ main.js
@@ -8,16 +8,34 @@ for (let slide of slides) {
     slide.addEventListener('change', createBalls);
 }
 
-function calculateRangeValues() {
-    let min = slides[0].value < slides[1].value ? +slides[0].value : +slides[1].value;
-    let diff = Math.abs(slides[0].value - slides[1].value);
+function calculateRangeValues(colorProp) {
+    let min;
+    let diff;
+
+    switch (colorProp) {
+        case "hue":
+            min = slides[0].value < slides[1].value ? +slides[0].value : +slides[1].value;
+            diff = Math.abs(slides[0].value - slides[1].value);
+            break;
+
+        case "saturation":
+            min = slides[2].value < slides[3].value ? +slides[2].value : +slides[3].value;
+            diff = Math.abs(slides[2].value - slides[3].value);
+            break;
+
+        case "lightness":
+            min = slides[4].value < slides[5].value ? +slides[4].value : +slides[5].value;
+            diff = Math.abs(slides[4].value - slides[5].value);
+            break;
+    }
+
     return { diff: diff, min: min }
 }
 
 function getRandomColor() {
-    let hue = Math.floor(Math.random() * calculateRangeValues()['diff']) + calculateRangeValues()['min'];
-    let saturation = Math.floor(Math.random() * 100);
-    let lightness = Math.floor(Math.random() * 50) + 25;
+    let hue = Math.floor(Math.random() * calculateRangeValues("hue")['diff']) + calculateRangeValues("hue")['min'];
+    let saturation = Math.floor(Math.random() * calculateRangeValues("saturation")['diff']) + calculateRangeValues("saturation")['min'];
+    let lightness = Math.floor(Math.random() * calculateRangeValues("lightness")['diff']) + calculateRangeValues("lightness")['min'];
 
     return 'hsl(' + hue + ', ' + saturation + '%, ' + lightness + '%)';
 }
