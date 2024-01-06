5249d803ca6f13970b48f099865a5e407e31c26d
boswel <s.luerzel@gmail.com>
2024-01-06 12:08:54 +0100

    minor improvements

--- ColorControls.js
+++ ColorControls.js
@@ -1,14 +1,14 @@
 export class ColorControls {
 
-    //put slides as property, constructor, let new ColControls in main
+    // TODO: put slides as property, constructor, let new ColControls in main
     static averageHue;
 
-    static HUE = 'hue'; // Q: I forgot why the varnames are in uppercase 
-    // and why they are given their names in lowercase as values;
-    // was it because it needed to be clear what values colorProp can take?
+    static HUE = 'hue'; // varnames in uppercase because they are used like constants
+    // because they define which values colorProp can take
     static SATURATION = 'saturation';
     static LIGHTNESS = 'lightness';
-    static alpha; // Q: 
+
+    static alpha;
 
     static getColorPropSlides(colorProp) {
         let selector = 'input.' + colorProp;
@@ -37,7 +37,7 @@ export class ColorControls {
             this.calculateRandom(this.SATURATION) + '%, ' +
             this.calculateRandom(this.LIGHTNESS) + '%, ' +
             this.alpha +
-            ')'; // Q: and I forgot why returning a string here is useful; is the function that is in the string called immediately?
+            ')';
     }
 
 }
